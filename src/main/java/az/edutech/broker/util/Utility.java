1f07618eaed2ad80ad403c49e513c841bc8cf046
soldev21 <sherif.huseynli@gmail.com>
2020-06-18 16:30:40 +0400

    first

---
+++ src/main/java/az/edutech/broker/util/Utility.java
@@ -0,0 +1,7 @@
+package az.edutech.broker.util;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Utility {
+    public static final ObjectMapper mapper = new ObjectMapper();
+}
